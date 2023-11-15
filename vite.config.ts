0cae6c632fc71c200558429087c3193dff6669f9
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-11-15 16:40:52 +0200

    init: react-ts eslint prettier husky redux toolkit router sass

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import * as path from "path";
+
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
+});

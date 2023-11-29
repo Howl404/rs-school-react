1d194bd2f9d027f195a35d267dbbae1898694c0f
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-11-30 00:19:35 +0200

    feat: remove not used alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import * as path from 'path';
-
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
@@ -7,9 +5,4 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
 });
